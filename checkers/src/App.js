b8e6e8b1ee29333a1eb6cf9d56b4b3e13b9e767e
Enragedtoast <edwardsjtimothy@gmail.com>
2019-10-24 19:43:49 -0500

    added bootstrap

--- checkers/src/App.js
+++ checkers/src/App.js
@@ -1,105 +1,115 @@
 import React from 'react';
 import './App.css';
+import "./components/css/style.css";
 
 function App() {
 
   return (
     <div className="App">
 
-      {/* Row A */}
+      <div className="container"></div>
       <div className="row">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
 
-      {/* Row B */}
-      <div className="row">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+        <div className="col col-sm-1 col-md-1 col-lg-1"></div>
+        <div className="col col-sm-10 col-md-10 col-lg-10">
 
-      {/* Row C */}
-      <div className="row">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+          {/* Row A */}
+          <div className="row">
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+          </div>
 
-      {/* Row D */}
-      <div className="row">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+          {/* Row B */}
+          <div className="row">
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+          </div>
 
-      {/* Row E */}
-      <div className="row">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+          {/* Row C */}
+          <div className="row">
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+          </div>
 
-      {/* Row F */}
-      <div className="row">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+          {/* Row D */}
+          <div className="row">
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+          </div>
 
-      {/* Row G */}
-      <div className="row">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+          {/* Row E */}
+          <div className="row">
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+          </div>
 
-      {/* Row H */}
-      <div className="row">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+          {/* Row F */}
+          <div className="row">
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+          </div>
+
+          {/* Row G */}
+          <div className="row">
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+          </div>
+
+          {/* Row H */}
+          <div className="row">
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+            <div className="board black"></div>
+            <div className="board red"></div>
+          </div>
+        </div>
+        <div className="col col-sm-1 col-md-1 col-lg-1"></div>
       </div>
     </div>
   );
