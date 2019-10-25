0db53ed4fb0f05753696c23fec354479e1721f48
Enragedtoast <edwardsjtimothy@gmail.com>
2019-10-24 19:27:47 -0500

    hard coding game board

--- checkers/src/App.js
+++ checkers/src/App.js
@@ -1,12 +1,106 @@
 import React from 'react';
 import './App.css';
-import Board from './components/board';
 
 function App() {
 
   return (
     <div className="App">
-      <Board> </Board>
+
+      {/* Row A */}
+      <div className="row">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Row B */}
+      <div className="row">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Row C */}
+      <div className="row">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Row D */}
+      <div className="row">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Row E */}
+      <div className="row">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Row F */}
+      <div className="row">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Row G */}
+      <div className="row">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Row H */}
+      <div className="row">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
