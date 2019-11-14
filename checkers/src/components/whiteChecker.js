ceffc47a2b0b06be3e56f0ed380a1bc1c5d61c3f
Enragedtoast <edwardsjtimothy@gmail.com>
2019-11-14 16:51:20 -0600

    working to use refs to focus selected checker

--- checkers/src/components/whiteChecker.js
+++ checkers/src/components/whiteChecker.js
@@ -3,26 +3,33 @@ import React, { Component } from 'react'
 
 
 export default class whiteChecker extends Component {
+    constructor(props) {
+        super(props);
+        this.hereIAm = React.createRef();
+        this.focusHereIAm = this.focusHereIAm.bind(this);
+      }
     
-    state = {
-        // A1: true,
-        // A3: true,
-        // A5: true,
-        // A7: true,
-        // B2: true,
-        // B4: true,
-        // B6: true,
-        // B8: true,
-        // C1: true,
-        // C3: true,
-        // C5: true,
-        // C7: true
-    }
+    
+    // state = {
+    //     A1: true,
+    //     A3: true,
+    //     A5: true,
+    //     A7: true,
+    //     B2: true,
+    //     B4: true,
+    //     B6: true,
+    //     B8: true,
+    //     C1: true,
+    //     C3: true,
+    //     C5: true,
+    //     C7: true
+    // }
 
     //function for selecting and moving white checkers
     pieceSelect=(e)=> {
-        let piece = e.target
-        piece.focus();
+        // let piece = e.target
+        // piece.focus();
+        this.hereIAm.current.focus();
         let startingPosition = e.target.parentNode.id;
         console.log(startingPosition);
         this.setState({ startingPosition });
@@ -30,7 +37,7 @@ export default class whiteChecker extends Component {
 
     render() {
         return (
-            <div className="checker white" onClick={this.pieceSelect}>
+            <div className="checker white" onClick={this.pieceSelect} ref={this.hereIAm} >
             </div>
         )
     }
