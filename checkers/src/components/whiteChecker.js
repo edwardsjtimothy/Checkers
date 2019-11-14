f5bae6ebf131e2a6e37a1873f8bc5e2328e15f6b
Enragedtoast <edwardsjtimothy@gmail.com>
2019-11-13 22:36:16 -0600

    gathering data value of parent div on click. Working on state

--- checkers/src/components/whiteChecker.js
+++ checkers/src/components/whiteChecker.js
@@ -2,15 +2,29 @@ import "./css/style.css"
 import React, { Component } from 'react'
 
 
-const pieceSelect=()=> {
-    console.log("clicked");
+const pieceSelect=(e)=> {
+    console.log(e.target.parentNode.id);
 }
 export default class whiteChecker extends Component {
+
+    state = {
+        whiteOne: "A1",
+        whiteTwo: "A3",
+        whiteThree: "A5",
+        whiteFour: "A7",
+        whiteFive: "B2",
+        whiteSix: "B4",
+        whiteSeven: "B6",
+        whiteEight: "B8",
+        whiteNine: "C1",
+        whiteTen: "C3",
+        whiteEleven: "C5",
+        whiteTwelve: "C7"
+    }
 
     render() {
         return (
             <div className="checker white" onClick={pieceSelect}>
-            
             </div>
         )
     }
