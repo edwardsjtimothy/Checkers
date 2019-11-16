4ff15b81917d409ce6370d1f20d64fd26864d99a
Enragedtoast <edwardsjtimothy@gmail.com>
2019-11-15 23:39:31 -0600

    cleaned up comments

--- checkers/src/components/whiteChecker.js
+++ checkers/src/components/whiteChecker.js
@@ -9,22 +9,6 @@ export default class whiteChecker extends Component {
         this.focusHereIAm = this.focusHereIAm.bind(this);
       }
     
-    
-    // state = {
-    //     A1: true,
-    //     A3: true,
-    //     A5: true,
-    //     A7: true,
-    //     B2: true,
-    //     B4: true,
-    //     B6: true,
-    //     B8: true,
-    //     C1: true,
-    //     C3: true,
-    //     C5: true,
-    //     C7: true
-    // }
-
     //function for selecting and moving white checkers
     pieceSelect=(e)=> {
         // let piece = e.target
