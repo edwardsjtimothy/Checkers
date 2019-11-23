01abd02538738dd307b773cb2d2852be04569466
Enragedtoast <edwardsjtimothy@gmail.com>
2019-11-23 16:07:49 -0600

    working on function to focus on clicked checker

--- checkers/src/components/whiteChecker.js
+++ checkers/src/components/whiteChecker.js
@@ -6,22 +6,30 @@ export default class whiteChecker extends Component {
     constructor(props) {
         super(props);
         this.hereIAm = React.createRef();
-        this.focusHereIAm = this.focusHereIAm.bind(this);
+        this.pieceSelect = this.pieceSelect.bind(this);
       }
-    
+
+    // componentDidUpdate() {
+    //     console.log(this.hereIAm.current.focus());
+    // }  
+
+    onFocus() {
+        document.body.style.backgroundColor = "red"
+    };
+
     //function for selecting and moving white checkers
-    pieceSelect=(e)=> {
-        // let piece = e.target
-        // piece.focus();
-        this.hereIAm.current.focus();
+    pieceSelect=(e)=> { 
+        // let focus =this.hereIAm.current.focus(e); 
+        e.target.focus()
         let startingPosition = e.target.parentNode.id;
+        // console.log(focus);
         console.log(startingPosition);
         this.setState({ startingPosition });
     }
 
     render() {
         return (
-            <div className="checker white" onClick={this.pieceSelect} ref={this.hereIAm} >
+            <div className="checker white" onClick={ this.pieceSelect} onFocus={this.onFocus} ref={this.hereIAm} >
             </div>
         )
     }
